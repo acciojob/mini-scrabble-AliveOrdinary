c32ad86d63ba02048f24389e4924f83f0b6356e7
Navneeth <vrnavaneeth135@gmail.com>
2022-12-28 16:44:00 +0530

    final

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+
+
+let string = document.getElementById("evaluatedText");
+let count = document.getElementById("letterCount");
+
+string.addEventListener('keyup',()=>{
+    let cnt = string.value;
+    count.innerText = cnt.length;
+})
